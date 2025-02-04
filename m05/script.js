491bd032fc34509427e4278c7a59b7f14d53115d
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-03 20:36:47 -0800

    Add prompt

--- m05/script.js
+++ m05/script.js
@@ -1,4 +1,4 @@
-var table = 3;             // Unit of table
+var table = prompt("Please input a value");         // Unit of table
 var operator = 'addition'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
