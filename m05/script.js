aab6a15e8c1e664e2fa21dca341f27f14059cc17
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-05 09:16:58 -0800

    Convert prompt to number

--- m05/script.js
+++ m05/script.js
@@ -1,7 +1,7 @@
-var table = prompt("Please input a value between 1 and 10");// Used for multiplication table
-var operator = 'multiplication';                            // Type of calculation
-var i = 1;                                                  // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';                  // Message initialization
+var table = Number(prompt("Please input a value between 1 and 10"));// Used for multiplication table
+var operator = 'multiplication';                                    // Type of calculation
+var i = 1;                                                          // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';                          // Message initialization
 
 // Checks if table is falsy or if value is less than 1 or greater than 10, will set value to 0
 if (!table || table < 1 || table > 10) {
