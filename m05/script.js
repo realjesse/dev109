ec9674eb1112c43c23b364a602f7cffed9806ef3
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-05 09:09:18 -0800

    Check if input is falsy
    We want any falsy value to be set to
    0 for the calculation.

--- m05/script.js
+++ m05/script.js
@@ -1,10 +1,16 @@
 var table = prompt("Please input a value");         // Used for multiplication table
-var operator = 'multiplication'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';              // Message
+var operator = 'multiplication';                    // Type of calculation
+var i = 1;                                          // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';          // Message
+
+// Checks if table is falsy, will set value to 0
+if (!table) {
+  table = 0;
+}
 
 if (operator === 'addition') {
-  // Do addition, commented out, don't need
+  // commented out, don't need
+  // Do addition
   //while (i < 11) {
   //  msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
   //  i++;
