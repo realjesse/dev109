15b737b62326885b35ad8fe3d9460f3ff1c70822
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-10 15:12:39 -0800

    Create downLeft();

--- a3/script.js
+++ a3/script.js
@@ -7,6 +7,7 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
   upRight(pHeight, pColorEven, pColorOdd, pSymbol);
   downRight(pHeight, pColorEven, pColorOdd, pSymbol);
   upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+  downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -100,4 +101,41 @@ function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
   }
 
   document.getElementById("upLeft").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+  var rLine ="";
+  // Start i = 1, because we subtract it from pHeight to determine if a space should be
+  // blank
+  for (i = 0; i < pHeight; i++){
+    rLine +="<p>";
+    
+    //Create each line on the Rhombus
+    for(j = 0; j < pHeight; j++){
+
+      // Determines if space should be blank, we have to add spaces before symbols to "fill out"
+      // the rhombus, so this if statement will fill in the negative space by determining how long
+      // the spaces should be repeated
+      if (j < i) {
+        rLine +="&nbsp;";
+      }
+      
+      //Is the position even or odd so we change the color
+      //even
+      else if (j % 2) {
+      rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+      }
+      
+      //odd
+      else {
+      rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+      }
+
+    }
+    rLine +="</p>";
+    // console.log(rLine);
+
+  }
+
+  document.getElementById("downLeft").innerHTML = rLine;
 }
