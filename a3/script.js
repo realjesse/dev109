68e0437e54935c0cb0fb8c31537f951a79329a26
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-10 14:13:40 -0800

    Upload template
    Template from assignment resources.

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,65 @@
+// var rHeight =5;
+// var colorEven = "orange";
+// var colorOdd = "black";
+// var symbol ="*";
 
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+  upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+  downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
+  var rLine ="";
+  for (i=0;i<pHeight;i++){
+    rLine +="<p>";
+    
+      //Create each line on the Rhombus
+      for(j=0;j<=i;j++){
+
+        //Is the position even or odd so we change the color
+        //even
+        if (j%2) {
+        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+        }
+        
+        //odd
+        else {
+        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+        }
+
+      }
+      rLine +="</p>";
+      // console.log(rLine);
+
+    }
+
+  document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+  var rLine ="";
+  for (i=pHeight;i > 0;i--){
+    rLine +="<p>";
+    
+    //Create each line on the Rhombus
+    for(j=0;j<i;j++){
+
+      //Is the position even or odd so we change the color
+      //even
+      if (j%2) {
+      rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+      }
+      
+      //odd
+      else {
+      rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+      }
+
+    }
+    rLine +="</p>";
+    // console.log(rLine);
+
+  }
+
+  document.getElementById("downRight").innerHTML = rLine;
+}
