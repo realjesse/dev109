00e62e8b1040b428dc76f0673afafa5d6a3c2de0
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-25 13:48:07 -0800

    Create getEmailValidity()

--- a4/js/script.js
+++ a4/js/script.js
@@ -1,5 +1,5 @@
 function isValid() {
-  if (getFirstNameValidity()) {
+  if (getFirstNameValidity() && getLastNameValidity()) {
     return true;
   }
   else {
@@ -39,7 +39,7 @@ function getFirstNameValidity() {
   //4) Send error message to HTML
   document.getElementById("firstNameWarning").innerHTML = errorMessages;
 
-  //5) return status of each field
+  //5) return status of isFirstNameValid
   return isFirstNameValid;
 };
 
@@ -73,9 +73,51 @@ function getLastNameValidity() {
   // Send error messages to HTML
   document.getElementById("lastNameWarning").innerHTML = errorMessages;
   
-  // Return status of each field
+  // Return status of lastNameValid
   return isLastNameValid;
+}
+
+function getEmailValidity() {
+  // create variables
+  let isEmailValid = false;
   
+  // Read values from HTML
+  let email = document.getElementById("email").value;
+  let errorMessages = "";
+  
+  // Find positions of @ and . to ensure validity of email later
+  let atPosition = email.indexOf("@");
+  let dotPosition = email.lastIndexOf(".");
+  
+  //Do validation
+  // If the email value is blank or null, give an error
+  if (email === null || email === "") {
+    errorMessages += "<p>The email is required, please enter one</p>";
+    console.log("Email invalid - not inputted");
+  }
+  
+  // The email must contain at least one "@" symbol and one "." symbol, if not give error
+  else if (!email.includes("@") && !email.includes(".")) {
+    errorMessages += "<p>The email must be valid.  Ensure you include an @ symbol and a . symbol</p>";
+    console.log("Email invalid - @ or . not found");
+  }
+  
+  // The "@" cannot be next to the ".", if it is then give an error
+  else if (atPosition < 1 || dotPosition < atPosition + 2 || dotPosition + 2 >= email.length) {
+    errorMessages += "<p>The email must be valid.  Ensure you the placement of the @ symbol and the . symbol are correct";
+    console.log("Email invalid - @ or . in incorrect position");
+  }
+  
+  else {
+    isEmailvalid = true;
+    console.log("Email is valid");
+  }
+  
+  // Send error messages to HTML
+  document.getElementById("emailWarning").innerHTML = erorrMessages;
+  
+  // Return status of emailValidity
+  return isEmailValid;
 }
 
 // Add event listeners for each field, checks if the input is valid
