068f1bf2efda4e3bbbc97e4af090140d43ff85ff
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-25 13:30:27 -0800

    Add rest of getFirstNameValidity() functionality

--- a4/js/script.js
+++ a4/js/script.js
@@ -18,14 +18,19 @@ function getFirstNameValidity() {
   let errorMessages = "";
 
   //3) Do validation
-  if (firstName === "null" || firstName === "" || firstName.length > 20 ) {
+  // If first name is null, empty, or longer than 20 chars, give error message
+  if (firstName === null || firstName === "" || firstName.length > 20 ) {
     errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
     console.log("First name invalid — length")
   } 
+  
+  // If first name has any non alphebetical character, give error message
   else if (firstName.match("^[a-zA-Z ,.'-]+$") === null) {
-    errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+    errorMessages += "<p>Invalid caracter in first name (accepts only A-Z, a-z, and ,.'-)</p>";
     console.log("First name invalid — bad characters")
   } 
+  
+  // If it passed the above, then it is valid!
   else {
     validFirstName = true;
     console.log("First name valid")
@@ -43,6 +48,34 @@ function getLastNameValidity() {
   let isvalid = false;
   
   // Read values from HTML
+  let lastName = document.getElementById("lastName").value;
+  let errorMessages = "";
+  
+  // Do validation
+  // If last name is null, empty, or longer than 20 chars, give error message
+  if (lastName === null || lastName === "" || lastName.length > 20) {
+    errorMessages += "<p>The last name is required and cannot be greater than 20 characters</p>";
+    console.log("Last name invalid - length");
+  }
+  
+  // If last name has any non alphebetical character, give error message
+  else if (lastName.match("^[a-zA-Z ,.'-]+$") === null) {
+    errorMessages += "<p>Invalid character in the last name (accepts only A-Z, a-z, and ,.'-)</p>";
+    console.log("Last name invalid - bad characters");
+  }
+  
+  // If it passed the above, then it is valid!
+  else {
+    validLastName = true;
+    console.log("Last name valid");
+  }
+  
+  // Send error messages to HTML
+  document.getElementById("lastNameWarning").innerHTML = errorMessages;
+  
+  // Return status of each field
+  return isvalid;
+  
 }
 
 // Add event listeners for each field, checks if the input is valid
