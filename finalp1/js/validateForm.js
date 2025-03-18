345962825b349ebe08d8d9a9ef09833bb96d185b
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-17 21:36:26 -0700

    Create validateData()

--- finalp1/js/validateForm.js
+++ finalp1/js/validateForm.js
@@ -4,3 +4,17 @@ let nameNode = document.getElementById('name');
 let emailNode = document.getElementById('email');
 let commentNode = document.getElementById('comment')
 const submitButtonNode = document.getElementById('submitButton')
+
+function validateData() {
+    if (nameNode.value === "" || nameNode.value === null) {
+        nameNode.style.borderColor = "red";
+    }
+
+    else if (emailNode.value === "" || emailNode.value === null) {
+        emailNode.style.borderColor = "red";
+    }
+
+    else if (commentNode.value === "" || commentNode.value === null) {
+        commentNode.style.borderColor = "red";
+    }
+}
