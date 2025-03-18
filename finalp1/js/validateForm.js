fbd729e1ec6dc07966c20bdad0a4d73ae4db6d54
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-17 21:41:54 -0700

    Fix path typo, addEventListener to form

--- finalp1/js/validateForm.js
+++ finalp1/js/validateForm.js
@@ -5,17 +5,22 @@ let emailNode = document.getElementById('email');
 let commentNode = document.getElementById('comment')
 const formNode = document.getElementById('contactForm')
 
-function validateData() {
+formNode.addEventListener("submit", validateData);
+
+// function to validate data on page
+function validateData(event) {
     if (nameNode.value === "" || nameNode.value === null) {
         nameNode.style.borderColor = "red";
-        
+        event.preventDefault();
     }
 
     else if (emailNode.value === "" || emailNode.value === null) {
         emailNode.style.borderColor = "red";
+        event.preventDefault();
     }
 
     else if (commentNode.value === "" || commentNode.value === null) {
         commentNode.style.borderColor = "red";
+        event.preventDefault();
     }
 }
