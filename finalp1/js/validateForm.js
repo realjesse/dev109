57dd71017772e7e4c9bbe51113252cdf979eab73
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-17 21:33:15 -0700

    Create variables

---
+++ finalp1/js/validateForm.js
@@ -0,0 +1,6 @@
+
+// Create variables
+let nameNode = document.getElementById('name');
+let emailNode = document.getElementById('email');
+let commentNode = document.getElementById('comment')
+const submitButtonNode = document.getElementById('submitButton')
